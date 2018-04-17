cb4bbe54ffbd71c77b0efa141ac519cd6cd03214
Alexander Schranz <alexander.schranz@sulu.io>
2018-04-17 17:16:25 +0200

    initialize basic stylelint config (#1)

---
+++ index.js
@@ -0,0 +1,118 @@
+"use strict"
+
+module.exports = {
+    "extends": "stylelint-config-standard",
+    "plugins": [
+        "stylelint-scss"
+    ],
+    "ignoreFiles": [
+        "sass/vendor/*.scss"
+    ],
+    "rules": {
+        "at-rule-empty-line-before": [
+            "always",
+            {
+                "except": [
+                    "blockless-after-same-name-blockless",
+                    "first-nested"
+                ],
+                "ignore": [
+                    "after-comment"
+                ],
+                "ignoreAtRules": [
+                    "extend",
+                    "include"
+                ]
+            }
+        ],
+        "at-rule-no-unknown": [
+            true,
+            {
+                "ignoreAtRules": [
+                    "content",
+                    "extend",
+                    "include",
+                    "mixin"
+                ]
+            }
+        ],
+        "function-comma-newline-before": "never-multi-line",
+        "function-name-case": [
+            "lower",
+            {
+                "ignoreFunctions": [
+                    "/^[a-z][a-z-A-Z0-9]*$/"
+                ]
+            }
+        ],
+        "string-quotes": "single",
+        "value-keyword-case": "lower",
+        "declaration-empty-line-before": "never",
+        "selector-attribute-quotes": "always",
+        "selector-class-pattern": "^[a-z][a-z-A-Z0-9]*$",
+        "selector-max-compound-selectors": [
+            3,
+            {
+                "severity": "warning"
+            }
+        ],
+        "selector-max-id": 0,
+        "selector-max-type": [
+            2,
+            {
+                "severity": "warning"
+            }
+        ],
+        "selector-max-combinators": [
+            2,
+            {
+                "severity": "warning"
+            }
+        ],
+        "selector-max-universal": 1,
+        "selector-max-specificity": "0,3,0",
+        "selector-list-comma-newline-before": "never-multi-line",
+        "selector-list-comma-space-after": "always-single-line",
+        "custom-media-pattern": "^[a-z][a-z-A-Z0-9]*$",
+        "media-query-list-comma-newline-before": "never-multi-line",
+        "at-rule-semicolon-space-before": "never",
+        "indentation": 4,
+        "max-line-length": 120,
+        "no-descending-specificity": [
+            true,
+            {
+                "severity": "warning"
+            }
+        ],
+        "no-duplicate-selectors": true,
+        "no-empty-source": null,
+        "block-no-empty": null,
+        "color-hex-length": "long",
+        "color-hex-case": ["upper"],
+        "shorthand-property-no-redundant-values": null,
+        "scss/dollar-variable-pattern": "^[a-z][a-z-A-Z0-9]*$",
+        "scss/dollar-variable-colon-space-after": "always-single-line",
+        "scss/dollar-variable-colon-space-before": "never",
+        "scss/dollar-variable-empty-line-before": [
+            "always",
+            {
+                "except": [
+                    "first-nested",
+                    "after-comment",
+                    "after-dollar-variable"
+                ]
+            }
+        ],
+        "scss/percent-placeholder-pattern": "^[a-z][a-z-A-Z0-9]*$",
+        "scss/double-slash-comment-whitespace-inside": "always",
+        "scss/declaration-nested-properties": "never",
+        "scss/operator-no-newline-after": true,
+        "scss/operator-no-newline-before": true,
+        "scss/operator-no-unspaced": true,
+        "scss/at-else-if-parentheses-space-before": "always",
+        "scss/at-function-parentheses-space-before": "never",
+        "scss/at-function-pattern": "^[a-z][a-z-A-Z0-9]*$",
+        "scss/at-mixin-parentheses-space-before": "never"
+    }
+}
+
