551cd290fdc17e1170df4bfe0f38193aae558312
Alexander Schranz <alexander@sulu.io>
2019-05-24 11:08:39 +0200

    Use numeric values for font-eight

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ module.exports = {
                 "ignoreAtRules": [ "if", "else", "elseif" ]
             }
         ],
-        "font-weight-notation": "named-where-possible",
+        "font-weight-notation": "numeric",
         "string-quotes": "single",
         "value-keyword-case": "lower",
         "declaration-empty-line-before": "never",
